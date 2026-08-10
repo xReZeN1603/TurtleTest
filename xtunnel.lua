eeee69b30a4db8c060d5455dcfccde27f6c9c3d7
xReZeN1603 <celvin.lepper@gmx.net>
2026-08-11 01:55:05 +0200

    Update xtunnel.lua

--- xtunnel.lua
+++ xtunnel.lua
@@ -107,7 +107,7 @@ for i = 1, math.ceil((length - 1) / 2) do
 -- ------------------------
 
 --Alle 10 Durchlaeufe Inventar leeren
-    if count % 20 == 1 or count == length * 2 - 1 then
+    if count % 20 == 19 or count == length * 2 - 1 then
         
         print("" .. count .. " Durchlaeufe erreicht!")
         print("Inventar wird geleert.")
