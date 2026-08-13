eca003fb450cb0809b8e61e0790fd490e8488d77
xReZeN1603 <celvin.lepper@gmx.net>
2026-08-13 02:03:35 +0200

    Update QuarryTurtle.lua

--- QuarryTurtle.lua
+++ QuarryTurtle.lua
@@ -13,3 +13,118 @@ local depth = tonumber(args[1])
 local length = tonumber(args[2])
 local width = tonumber(args[3])
 
+--Wartet bis abgebaut ist, faehrt 1 vor
+local function digMove()
+    turtle.dig()
+    sleep(0.3)
+    turtle.digDown()
+    sleep(0.3)
+
+    while not turtle.forward() do
+        turtle.dig()
+        sleep(0.3)
+        turtle.digDown()
+        sleep(0.3)
+    end
+
+end
+
+
+
+-- VORBEREITUNG
+
+for k = 1, depth do
+
+  turtle.digDown()
+  sleep(0.3)
+  turtle.down()
+  sleep(0.3)
+
+
+  for i = 1, length do
+
+    digMove()
+
+  end
+
+----<<<<<Breite
+  for j = 1, width / 2 do
+
+    turtle.turnRight()
+    digMove()
+    turtle.turnRight()
+
+    for i = 1, length do 
+
+      digMove()
+      sleep(0.3)
+
+    end
+
+
+    turtle.turnLeft()
+    sleep(0.3)
+    digMove()
+    sleep(0.3)
+    turtle.turnLeft()
+    sleep(0.3)
+
+    for i = 1, length do
+
+      digMove()
+      sleep(0.3)
+
+    end
+  
+  end
+
+  turtle.turnLeft()
+  sleep(0.3)
+
+  for j = 1, width * 2 do
+
+    turtle.forward()
+    sleep(0.3)
+
+  end
+
+  turtle.turnLeft()
+
+  for i = 1, length do
+
+    turtle.forward()
+    sleep(0.3)
+
+  end
+
+
+-- FLÄCHE WEG, STEHT VOR KISTE
+
+  turtle.turnLeft()
+  sleep(0.3)
+  turtl.turnLeft()
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
