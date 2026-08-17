0cf0dbace2d0bbd902d6796d3f7eee0fef2b43c5
xReZeN1603 <celvin.lepper@gmx.net>
2026-08-17 02:36:07 +0200

    Create CC:TweakendMonitor.lua

---
+++ CC:TweakendMonitor.lua
@@ -0,0 +1,14 @@
+m=peripheral.find("monitor")
+m.setTextScale(2)
+m.clear()
+
+m.setBackgroundColor(colors.white)
+m.clearLine()
+
+m.setCursorPos(2,2)
+m.setBackgroundColor(colors.lime)
+m.setTextColor(colors.lime)
+m.write("Lagerraum")
+
+m.setBackgroundColor(colors.white)
+m.setTextColor(colors.white)
