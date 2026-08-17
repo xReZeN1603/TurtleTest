cfb31d391384fd71acbce7ca4eeb7fdab48f63b7
xReZeN1603 <celvin.lepper@gmx.net>
2026-08-17 02:45:26 +0200

    Update CCTweakendMonitor.lua

--- CCTweakendMonitor.lua
+++ CCTweakendMonitor.lua
@@ -1,17 +1,28 @@
-Download: wget https://github.com/xReZeN1603/TurtleTest/main/CCTweakendMonitor.lua
-
+Download: wget https://raw.githubusercontent.com/xReZeN1603/TurtleTest/main/CCTweakendMonitor.lua m1
 
 m=peripheral.find("monitor")
 m.setTextScale(2)
 m.clear()
 
-m.setBackgroundColor(colors.white)
-m.clearLine()
+local w,h=m.getSize()
+local text="Lagerraum"
+local x=math.floor((w-#text)/2)+1
+local y=math.floor(h/2)
 
-m.setCursorPos(2,2)
-m.setBackgroundColor(colors.lime)
-m.setTextColor(colors.lime)
-m.write("Lagerraum")
-
-m.setBackgroundColor(colors.white)
 m.setTextColor(colors.white)
+
+m.setCursorPos(x-1,y-1)
+m.write("───────────")
+
+m.setCursorPos(x-1,y)
+m.write("│")
+
+m.setCursorPos(x+ #text,y)
+m.write("│")
+
+m.setCursorPos(x-1,y+1)
+m.write("───────────")
+
+m.setTextColor(colors.lime)
+m.setCursorPos(x,y)
+m.write(text)
